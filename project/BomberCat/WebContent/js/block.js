f44ffbc2d26f4ac8a9b0d80743105822ad5891dd
Jonas Knapp <de.jonas.knapp@gmail.com>
2016-05-30 13:49:09 +0200

    block

---
+++ project/BomberCat/WebContent/js/block.js
@@ -0,0 +1,13 @@
+function Block() {
+	this.x = 0;
+	this.y = 0;
+	this.sprite = new Sprite();
+
+	this.load = function(imgSrc) {
+		this.sprite.load(imgSrc);
+	}
+
+	this.draw = function(x, y) {
+		this.sprite.draw(x, y);
+	}
+}

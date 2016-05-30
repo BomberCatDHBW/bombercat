5f7048916b07a00409f31a5b4c696a5e3e0d4d5b
leonardo <reger@Leonardo-Laptop.reger.lan>
2016-05-30 11:38:56 +0200

    try

--- project/BomberCat/src/de/dhbwka/java/bombercat/servercalls/lobby/GetMap.java
+++ project/BomberCat/src/de/dhbwka/java/bombercat/servercalls/lobby/GetMap.java
@@ -1,23 +1,21 @@
 package de.dhbwka.java.bombercat.servercalls.lobby;
 
 import java.io.File;
-import java.io.IOException;
 
-import de.dhbwka.java.bombercat.BomberCatMap;
 import de.dhbwka.java.bombercat.Client;
 import de.dhbwka.java.bombercat.Lobby;
 
 public class GetMap implements LobbyCall {
 	@Override
 	public void run(String[] parameter, java.util.Map<String, Lobby> lobbies, Client client) {
-		ClassLoader classLoader = getClass().getClassLoader();
-		File mapFile = new File(classLoader.getResource("Maps/" + parameter[0]).getFile());
-		System.out.println(mapFile.getAbsolutePath());
-		try {
-			BomberCatMap map = new BomberCatMap(mapFile);
-			client.sendMessage(map.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		File f = new File("");
+		System.out.println(f.getAbsolutePath());
+		// System.out.println(mapFile.getAbsolutePath());
+		// try {
+		// BomberCatMap map = new BomberCatMap(mapFile);
+		// client.sendMessage(map.toString());
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
 	}
 }

55937d801d184743ce83b8bff25bd5c83e2166bb
leonardo <reger@Leonardo-Laptop.reger.lan>
2016-05-30 11:20:23 +0200

    changed system pathsep.

--- project/BomberCat/src/de/dhbwka/java/bombercat/servercalls/lobby/GetMap.java
+++ project/BomberCat/src/de/dhbwka/java/bombercat/servercalls/lobby/GetMap.java
@@ -3,14 +3,14 @@ package de.dhbwka.java.bombercat.servercalls.lobby;
 import java.io.File;
 import java.io.IOException;
 
+import de.dhbwka.java.bombercat.BomberCatMap;
 import de.dhbwka.java.bombercat.Client;
 import de.dhbwka.java.bombercat.Lobby;
-import de.dhbwka.java.bombercat.BomberCatMap;
 
 public class GetMap implements LobbyCall {
 	@Override
 	public void run(String[] parameter, java.util.Map<String, Lobby> lobbies, Client client) {
-		String path = "BomberCat\\Maps\\" + parameter[0];
+		String path = "BomberCat" + File.pathSeparator + "Maps" + File.pathSeparator + parameter[0];
 		File mapFile = new File(path);
 		System.out.println(mapFile.getAbsolutePath());
 		try {

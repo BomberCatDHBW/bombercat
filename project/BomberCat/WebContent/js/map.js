04d37d68dde91f033f9b0c85a8436e6507d1599e
Jonas Knapp <de.jonas.knapp@gmail.com>
2016-05-30 15:12:16 +0200

    Map-Feature Client, added crate Block

--- project/BomberCat/WebContent/js/map.js
+++ project/BomberCat/WebContent/js/map.js
@@ -23,20 +23,11 @@ function Map() {
 		this.width = object.width;
 		this.height = object.height;
 		this.fieldRefs = object.fieldRefs;
-		console.log(this.name + " " + this.width + "x" + this.height);
-
-		console.log(object.fieldRefs[0]);
-
-		console.log("Loading ParentBlocks!");
-		console.log(object.fieldRefs.length);
 		for (var i = 0; i < 3; i++) {			
 			var parentBlock = new Block();
 			parentBlock.load(object.fieldRefs[i]);
 			this.parentBlocks.push(parentBlock);
 		}
-		//parentBlock.load("http://vignette3.wikia.nocookie.net/nitromepixellove/images/a/a7/Red_Block.png/revision/latest?cb=20120618214011");
-		console.log("Loading Blocks!");
-
 		for (var x = 0; x < this.height; x++) {
 			for (var y = 0; y < this.width; y++) {
 				//console.log(object.field[y][x]);

547aa89a30058b332af59d943e5d124442dc3c5e
Jonas Knapp <de.jonas.knapp@gmail.com>
2016-07-28 12:47:56 +0200

    IP changed to localhost

--- project/BomberCat/WebContent/js/globalSettings.js
+++ project/BomberCat/WebContent/js/globalSettings.js
@@ -1,8 +1,8 @@
 var logServerMessages = false;
 var logSendMessages = false;
 
-//var serverIP = "localhost";
-var serverIP = "172.16.53.248";
+var serverIP = "localhost";
+//var serverIP = "172.16.53.248";
 
 var mainColorR = 204;
 var mainColorG = 0;
